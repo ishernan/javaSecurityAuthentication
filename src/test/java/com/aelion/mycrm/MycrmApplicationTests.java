ad9df1fa3c165742b406db54343b0eb18b66e348
ishernan <hernandez.isaac@gmail.com>
2021-12-09 11:39:49 +0100

    first commit

---
+++ src/test/java/com/aelion/mycrm/MycrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aelion.mycrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
