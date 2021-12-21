370be8af3d33e6d4253bd88ff0fccdfa27d22e6a
ishernan <hernandez.isaac@gmail.com>
2021-12-21 09:46:58 +0100

    apiSecurityConfig

--- src/main/java/com/aelion/mycrm/config/ApiSecurityConfig.java
+++ src/main/java/com/aelion/mycrm/config/ApiSecurityConfig.java
@@ -1,39 +1,80 @@
 package com.aelion.mycrm.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.RegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.aelion.mycrm.components.ApiAuthenticationEntryPoint;
+import com.aelion.mycrm.components.JwtAuthenticationFilter;
 import com.aelion.mycrm.services.UserAuthService;
 
 @Configuration
-public class ApiSecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserAuthService userAuthService;
-	
-	
-	
-//	@Autowired	
-//	public RegistrationBean jwtAuthFilterRegister(JwtAuthenticationFilter){
-//		
-//	}
-	
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManager();
-	}
-	
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	@Autowired
+	private ApiAuthenticationEntryPoint authenticationEntryPoint;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/user/signin", "/user/signup", "/user/secret");
+	}
+
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.userAuthService).passwordEncoder(this.passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().authorizeRequests()
+				.antMatchers("/user/signin", "/user/signup", "/user/secret")
+				.permitAll().anyRequest().authenticated()
+				.and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).
+				and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	  @Bean
+	    public RegistrationBean jwtAuthFilterRegister(JwtAuthenticationFilter filter) {
+	        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>(filter);
+	        registrationBean.setEnabled(false);
+	        return registrationBean;
+	    }
+	
+	
+	
+		
+	
+	
 }
